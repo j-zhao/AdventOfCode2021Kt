06578f4a50470fe848adce5dddc58e7ebb35750b
Jesse Zhao <jessezhao1992@gmail.com>
2021-12-01 15:09:01 -0800

    Implements Day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,40 @@
+import java.util.LinkedList
+
+/**
+ * https://adventofcode.com/2021/day/1
+ */
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var incrCount = -1
+        var lastDepth = -1
+        input.forEach {
+            if (it.toInt() > lastDepth) {
+                incrCount++
+            }
+            lastDepth = it.toInt()
+        }
+        return incrCount
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val depths = LinkedList<Int>()
+        var incrCount = 0
+        input.forEach {
+            val depth = it.toInt()
+            if (depths.size == 3) {
+                if (depth > depths.remove()) {
+                    incrCount++
+                }
+            }
+            depths.add(depth)
+        }
+        return incrCount
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
