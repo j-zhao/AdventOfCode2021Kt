90913e9cbdf520c143bcc0e1e729cc611adfd468
Jesse Zhao <jessezhao1992@gmail.com>
2021-12-02 09:29:37 -0800

    Day02 solution

--- src/Day02.kt
+++ src/Day02.kt
@@ -4,11 +4,35 @@
 
 fun main() {
     fun part1(input: List<String>): Int {
-
+        var horizontalPos = 0
+        var depth = 0
+        input.forEach {
+            val args = it.split(" ")
+            when (args[0]) {
+                "forward" -> horizontalPos += args[1].toInt()
+                "up" -> depth -= args[1].toInt()
+                "down" -> depth += args[1].toInt()
+            }
+        }
+        return horizontalPos * depth
     }
 
     fun part2(input: List<String>): Int {
-
+        var aim = 0
+        var horizontalPos = 0
+        var depth = 0
+        input.forEach {
+            val (dir, value) = it.split(" ")
+            when (dir) {
+                "forward" -> {
+                    depth += aim * value.toInt()
+                    horizontalPos += value.toInt()
+                }
+                "up" -> aim -= value.toInt()
+                "down" -> aim += value.toInt()
+            }
+        }
+        return horizontalPos * depth
     }
 
     val input = readInput("Day02")
