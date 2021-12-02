e806455a5d638c5237f683520fa8b858d24e9dd4
Jesse Zhao <jessezhao1992@gmail.com>
2021-12-02 09:23:28 -0800

    Day2 init

---
+++ src/Day02.kt
@@ -0,0 +1,17 @@
+/**
+ * https://adventofcode.com/2021/day/2
+ */
+
+fun main() {
+    fun part1(input: List<String>): Int {
+
+    }
+
+    fun part2(input: List<String>): Int {
+
+    }
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
