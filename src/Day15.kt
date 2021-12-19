1134c2b73ace611a87a5677354c1ebe95a9ba086
Jesse Zhao <jessezhao1992@gmail.com>
2021-12-19 13:28:49 -0800

    Day 15 in progress

--- src/Day15.kt
+++ src/Day15.kt
@@ -3,13 +3,21 @@ fun main() {
         val endpoint = Pair(input.size - 1, input[0].size - 1)
         val start = Pair(0, 0)
         val traversed = mutableSetOf<Set<Pair<Int, Int>>>()
+        var min = Int.MAX_VALUE
 
         fun sumRisk(visited: Set<Pair<Int, Int>>): Int {
             return visited.sumOf { input[it.first][it.second] } - input[0][0]
         }
 
         fun findMinRisk(xy: Pair<Int, Int>, visited: Set<Pair<Int, Int>>): Int? {
-            if (xy == endpoint) return sumRisk(visited) + input[xy.first][xy.second]
+            if (xy == endpoint) {
+                val currRisk = sumRisk(visited) + input[xy.first][xy.second]
+                if (currRisk < min) {
+                    min = currRisk
+                    println(min)
+                }
+                return currRisk
+            }
             val traversal = visited.plus(xy)
             traversed.add(traversal)
             val adjCoordinates = getAdjacentCoordinates(xy, input.size, input[0].size)
@@ -19,11 +27,18 @@ fun main() {
             val risks = adjCoordinates
                 .mapNotNull { findMinRisk(it, traversal) }
             if (risks.isEmpty()) return null
-            println(risks)
             return risks.minOrNull()
         }
+        val startMs = System.currentTimeMillis()
+        while (true) {
+            findMinRisk(start, setOf(start))?.let {
+                min = min.coerceAtMost(it)
+            }
+            println("Elapsed: ${System.currentTimeMillis() - startMs}")
+            if (System.currentTimeMillis() - startMs > 30 * 1000) break
+        }
 
-        return findMinRisk(start, setOf(start))
+        return min
     }
 
     val testInput = listOf(
